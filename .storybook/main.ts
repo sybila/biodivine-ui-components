2ed358d0067d7a78efb4c397b9cd2b7dcf16ec81
Samuel Pastva <sam.pastva@gmail.com>
2025-07-16 14:57:44 +0200

    chore: Apply `prettier` rules to `storybook` configuration.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,16 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/web-components-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {},
+  },
 };
 export default config;
