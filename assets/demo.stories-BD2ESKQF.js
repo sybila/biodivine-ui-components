a879d0fc224f586983e232450d2220513e571e93
daemontus <daemontus@gmail.com>
2025-07-29 14:10:15 +0000

    Deploying to gh-pages from @ sybila/biodivine-ui-components@33cf128d8581d80d778126ff317434774a084cc5 🚀

--- assets/demo.stories-BD2ESKQF.js
+++ assets/demo.stories-BD2ESKQF.js
@@ -1,4 +1,4 @@
-const{expect:s}=__STORYBOOK_MODULE_TEST__,r={title:"Demo",component:"my-label"},e={name:"Demo story"},t={name:"Simple test",play:async({userEvent:a})=>{await a.keyboard("abc");const o=document.querySelector("body");await s(o).toBeDisabled()}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+const{expect:s}=__STORYBOOK_MODULE_TEST__,r={title:"Demo",component:"my-label"},e={name:"Demo story"},t={name:"Simple test",play:async({userEvent:a})=>{await a.keyboard("abc");const o=document.querySelector("body");await s(o).toBeVisible()}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
   name: 'Demo story'
 }`,...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
   name: 'Simple test',
@@ -7,7 +7,6 @@ const{expect:s}=__STORYBOOK_MODULE_TEST__,r={title:"Demo",component:"my-label"},
   }) => {
     await userEvent.keyboard('abc'); // User is typing something mindlessly.
     const body = document.querySelector('body')!;
-    // This should intentionally fail to make sure CI is working correctly.
-    await expect(body).toBeDisabled();
+    await expect(body).toBeVisible();
   }
 }`,...t.parameters?.docs?.source}}};const n=["Default","Test"];export{e as Default,t as Test,n as __namedExportsOrder,r as default};
