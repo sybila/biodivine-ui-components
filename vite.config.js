322e00a48cad6192de8327c78abbb862bbc89765
Samuel Pastva <sam.pastva@gmail.com>
2025-07-29 15:04:26 +0200

    build: Update `vitest` configuration for the new `vite` config format.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   build: {
     target: 'esnext',
@@ -13,5 +22,39 @@ export default defineConfig({
       external: /^lit/,
     },
   },
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'unit',
+          root: './src',
+          environment: 'node',
+        }
+      }
+    ],
+  },
 });
-
