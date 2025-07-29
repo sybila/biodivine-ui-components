4e259c2e0753d5bc4d5fa88f66a3674c22030d04
Samuel Pastva <sam.pastva@gmail.com>
2025-07-29 15:38:48 +0200

    chore: Add a useless story for the purposes of testing storybook.

---
+++ src/components/demo.stories.ts
@@ -0,0 +1,26 @@
+// Same here... the story is useless, but we can use it to make sure everything is working as it should.
+
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { expect } from 'storybook/test';
+import { MyLabel } from './demo';
+
+const meta = {
+    title: 'Demo',
+    component: 'my-label',
+} satisfies Meta<typeof MyLabel>
+
+export default meta;
+ 
+export const Default: StoryObj<typeof meta> = {
+    name: 'Demo story',
+};
+
+export const Test: StoryObj<typeof meta> = {
+    name: 'Simple test',
+    play: async ({ canvas, userEvent }) => {
+        await userEvent.keyboard('abc') // User is typing something mindlessly.
+        const body = document.querySelector('body')!
+        // This should intentionally fail to make sure CI is working correctly.
+        await expect(body).toBeDisabled()
+    }
+}
