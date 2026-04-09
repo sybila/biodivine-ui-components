618641f58a5135560eb87e9949dcf195e28d00c8
MrMooshaber <vojtavesely1@seznam.cz>
2026-04-09 17:14:28 +0200

    chore: unused import removed

--- src/components/text/colored-words.ts
+++ src/components/text/colored-words.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, PropertyValues } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 type StyleProperty =
