bd43d6f73103e0cbab195453875a2d09e7eb5d40
MrMooshaber <162307484+MrMooshaber@users.noreply.github.com>
2026-09-01 19:18:02 +0000

    Deploying to gh-pages from @ sybila/biodivine-ui-components@ad782ffa9c6265a0937cdb241d67ff27ccdc7560 🚀

---
+++ feat/post-thesis-updates/assets/state-ze8viTcc.js
@@ -0,0 +1,5 @@
+import{n as r}from"./property-Dgah4u3S.js";/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */function n(t){return r({...t,state:!0,attribute:!1})}export{n as r};
